f43c9727ae27b6262c456c12932086385d618a2d
wd <1642297507@qq.com>
2019-12-07 16:11:34 +0800

    修复管理员bug

--- routes/board.py
+++ routes/board.py
@@ -17,9 +17,12 @@ bp_board = Blueprint('bp_board', __name__)
 
 def role_check():
     user = current_user()
-    if user.role == 1:
-        return True
-    return False
+    if user is None:
+        return False
+    else:
+        if user.role == 1:
+            return True
+        return False
 
 
 @bp_board.route('/admin')
